9c79bbff4bb19c7f2a4f1cf017f981c18d26583a
Denis Kolesnikov <exsdis@gmail.com>
2015-04-07 22:21:44 +0600

    Added ring and compojure

--- project.clj
+++ project.clj
@@ -5,9 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2069"]]
+                 [org.clojure/clojurescript "0.0-2069"]
+                 [compojure "1.1.6"]]
   
-  :plugins [[lein-cljsbuild "1.0.0"]]
+  :plugins [[lein-cljsbuild "1.0.0"]
+            [lein-ring "0.8.8"]]
+
+  :ring {:handler modern-cljs.core/handler}
   
   :cljsbuild {:builds
               [{
