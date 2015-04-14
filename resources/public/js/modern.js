4ead42bb67e0dab37b4d781f14cd19a1fa98ba53
Denis Kolesnikov <exsdis@gmail.com>
2015-04-14 21:13:58 +0600

    Some form validation

--- resources/public/js/modern.js
+++ resources/public/js/modern.js
@@ -28765,6 +28765,34 @@ goog.net.xpc.FrameElementMethodTransport.prototype.disposeInternal = function() 
   this.outgoing_ = null;
   this.iframeElm_ = null
 };
+goog.provide("modern_cljs.login");
+goog.require("cljs.core");
+modern_cljs.login.validate_form = function validate_form() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  if(cljs.core.count.call(null, email.value) > 0 && cljs.core.count.call(null, password.value) > 0) {
+    return true
+  }else {
+    alert("Please, complete the form!");
+    return false
+  }
+};
+modern_cljs.login.init = function init() {
+  if(cljs.core.truth_(function() {
+    var and__3273__auto__ = document;
+    if(cljs.core.truth_(and__3273__auto__)) {
+      return document.getElementById
+    }else {
+      return and__3273__auto__
+    }
+  }())) {
+    var login_form = document.getElementById("loginForm");
+    return login_form.onsubmit = modern_cljs.login.validate_form
+  }else {
+    return null
+  }
+};
+window.onload = modern_cljs.login.init;
 goog.provide("goog.Uri");
 goog.provide("goog.Uri.QueryData");
 goog.require("goog.array");
